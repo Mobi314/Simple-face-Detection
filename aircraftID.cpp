97cf2afc4af74e7864069f9b62f1132426ffb02e
Tahrim Imon <timon@stevens.edu>
2022-05-11 00:15:48 -0400

    Create aircraftID.cpp

---
+++ aircraftID.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+
+using namespace cv;
+using namespace std;
+
+
+int main(int argc, char* argv[]) {
+}
