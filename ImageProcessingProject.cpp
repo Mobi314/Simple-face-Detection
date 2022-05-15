f8586fbb2cc0efa766a8cc6f0ff6e94e5df87708
Tahrim Imon <68234338+Mobi314@users.noreply.github.com>
2022-05-14 23:11:46 -0400

    Update ImageProcessingProject.cpp

--- ImageProcessingProject.cpp
+++ ImageProcessingProject.cpp
@@ -16,61 +16,62 @@
 using namespace cv;
 using namespace std;
 
-void detectAndDraw(Mat& img, CascadeClassifier& cascade,
-    CascadeClassifier& nestedCascade,
-    double scale)
+void faceDetection(Mat& img, CascadeClassifier& cascade, CascadeClassifier& nestedCascade, double scale)
 {
-    vector<Rect> faces, faces2;
-    Mat gray, smallImg;
+    vector<Rect> faces1, faces2;
+    Mat grayScale, sm_img;
+    double fix = 1 / scale;
 
-    // Convert to Gray Scale
-    cvtColor(img, gray, COLOR_BGR2GRAY);
-    double fx = 1 / scale;
+    /*cvtColor function to convert the image to Grey Scale*/
+    cvtColor(img, grayScale, COLOR_BGR2GRAY);
 
-    // Resize the Grayscale Image 
-    resize(gray, smallImg, Size(), fx, fx, INTER_LINEAR);
-    equalizeHist(smallImg, smallImg);
+    /*Uniformally resize the Greyscale Image*/
+    resize(grayScale, sm_img, Size(), fix, fix, INTER_LINEAR);
+    equalizeHist(sm_img, sm_img);
 
-    // Detect faces of different sizes using cascade classifier 
-    cascade.detectMultiScale(smallImg, faces, 1.1,
+    /*Using the object detection library, we can detect faces
+    Using the cascade classifier*/
+    cascade.detectMultiScale(sm_img, faces1, 1.1,
         2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
 
-    // Draw circles around the faces
-    for (size_t i = 0; i < faces.size(); i++)
-    {
-        Rect r = faces[i];
-        Mat smallImgROI;
+    //Draw circles around the faces
+    for(size_t i = 0; i < faces1.size(); i++){
+        
         vector<Rect> nestedObjects;
         Point center;
-        Scalar color = Scalar(255, 0, 0); // Color for Drawing tool
+
+        Rect rect = faces1[i];
+        Mat sm_imgROI;
+        
+        Scalar color = Scalar(0, 255, 0); //Scalar Function Draws the color Green
         int radius;
 
-        double aspect_ratio = (double)r.width / r.height;
-        if (0.75 < aspect_ratio && aspect_ratio < 1.3)
-        {
-            center.x = cvRound((r.x + r.width * 0.5) * scale);
-            center.y = cvRound((r.y + r.height * 0.5) * scale);
-            radius = cvRound((r.width + r.height) * 0.25 * scale);
+        double aspect_ratio = (double)rect.width / rect.height;
+        
+        /*Draws circle around identified face
+        using circle function within a specified aspect ratio constraint*/
+        if(0.25 < aspect_ratio && aspect_ratio < 2){
+            center.x = cvRound((rect.x + rect.width * 0.5) * scale);
+            center.y = cvRound((rect.y + rect.height * 0.5) * scale);
+            radius = cvRound((rect.width + rect.height) * 0.25 * scale);
             circle(img, center, radius, color, 3, 8, 0);
         }
         else
-            rectangle(img, cv::Point(cvRound(r.x * scale), cvRound(r.y * scale)),
-                cv::Point(cvRound((r.x + r.width - 1) * scale),
-                    cvRound((r.y + r.height - 1) * scale)), color, 3, 8, 0);
-        if (nestedCascade.empty())
+            rectangle(img, cv::Point(cvRound(rect.x * scale), cvRound(rect.y * scale)),
+                cv::Point(cvRound((rect.x + rect.width - 1) * scale),
+                    cvRound((rect.y + rect.height - 1) * scale)), color, 3, 8, 0);
+        if(nestedCascade.empty())
             continue;
-        smallImgROI = smallImg(r);
+        sm_imgROI = sm_img(rect);
 
         // Detection of eyes int the input image
-        nestedCascade.detectMultiScale(smallImgROI, nestedObjects, 1.1, 2,
-            0 | CASCADE_SCALE_IMAGE, Size(30, 30));
+        nestedCascade.detectMultiScale(sm_imgROI, nestedObjects, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
 
-        // Draw circles around eyes
-        for (size_t j = 0; j < nestedObjects.size(); j++)
-        {
+        /*Like the facial draw, we draw circles around the eyes*/
+        for(size_t j = 0; j < nestedObjects.size(); j++){
             Rect nr = nestedObjects[j];
-            center.x = cvRound((r.x + nr.x + nr.width * 0.5) * scale);
-            center.y = cvRound((r.y + nr.y + nr.height * 0.5) * scale);
+            center.x = cvRound((rect.x + nr.x + nr.width * 0.5) * scale);
+            center.y = cvRound((rect.y + nr.y + nr.height * 0.5) * scale);
             radius = cvRound((nr.width + nr.height) * 0.25 * scale);
             circle(img, center, radius, color, 3, 8, 0);
         }
@@ -82,13 +83,14 @@ void detectAndDraw(Mat& img, CascadeClassifier& cascade,
 
 int main(int argc, const char** argv)
 {
-    // VideoCapture class for playing video for which faces to be detected
-    VideoCapture capture;
-    Mat frame, image;
+    /*VideoCapture class for capturing the frames in which
+    the faces will be detected and displayed*/
+    VideoCapture cap;
+    Mat frm, img;
 
     // PreDefined trained XML classifiers with facial features
     CascadeClassifier cascade, nestedCascade;
-    double scale = 1;
+    double sc = 1;
 
     // Load classifiers from "opencv/data/haarcascades" directory 
     nestedCascade.load("C:/OpenCV-4.5.5/opencv/sources/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml");
@@ -97,24 +99,23 @@ int main(int argc, const char** argv)
     cascade.load("C:/OpenCV-4.5.5/opencv/sources/data/haarcascades/haarcascade_frontalcatface.xml");
 
     // Start Video..1) 0 for WebCam 2) "Path to Video" for a Local Video
-    capture.open(0);
-    if (capture.isOpened())
+    cap.open(0);
+    if (cap.isOpened())
     {
         // Capture frames from video and detect faces
         cout << "Face Detection Started...." << endl;
-        while (1)
-        {
-            capture >> frame;
-            if (frame.empty())
+        while (1){
+            cap >> frm;
+            if (frm.empty())
                 break;
             
-            Mat frame1 = frame.clone();
-            detectAndDraw(frame1, cascade, nestedCascade, scale);
+            Mat frame1 = frm.clone();
+            faceDetection(frame1, cascade, nestedCascade, sc);
             
-            char c = (char)waitKey(10);
+            char keyPress = (char)waitKey(10);
 
             // Press q to exit from window
-            if (c == 27 || c == 'q' || c == 'Q')
+            if (keyPress == 27 || keyPress == 'q' || keyPress == 'Q')
                 break;
         }
     }
